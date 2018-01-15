1691e7f8052cd016a11e1b96235d9184ef649ba9
mr.gao <1451953172@qq.com>
2018-01-16 00:18:46 +0800

    添加了功能： 1、可添加EmptyView,ErrorView,HeaderView; 2、可使用不同的方式添加上述的view 3、添加默认的分割线；
    带已完成的功能：
    将SwipeRefreshLayout和LucklyRecyclerView合并

--- app/src/main/java/mrgao/com/recyclerviewtext/loadMore/activity/LoadingActivity.java
+++ app/src/main/java/mrgao/com/recyclerviewtext/loadMore/activity/LoadingActivity.java
@@ -1,15 +1,12 @@
 package mrgao.com.recyclerviewtext.loadMore.activity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,29 +14,35 @@ import java.util.List;
 import mrgao.com.recyclerviewtext.R;
 import mrgao.com.recyclerviewtext.loadMore.adapter.DataAdapter;
 import mrgao.com.recyclerviewtext.loadMore.recyclerview.YRecyclerView;
-import mrgao.com.recyclerviewtext.loadMore.views.YRecyclerViewItemDivider;
 
-public class LoadingActivity extends AppCompatActivity implements YRecyclerView.OnLoadMoreListener, SwipeRefreshLayout.OnRefreshListener {
+public class LoadingActivity extends AppCompatActivity implements YRecyclerView.OnLoadMoreListener, View.OnClickListener {
     String TAG = "LoadingActivity";
     YRecyclerView mYRecyclerView;
-    private SwipeRefreshLayout mSwipeRefreshLayout;
+
     DataAdapter dataAdapter;
+    Button mEmptyBtn, mErrorBtn, mRestoreBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
         mYRecyclerView = $(R.id.yRecyclerView);
-        mSwipeRefreshLayout = $(R.id.swipe);
-        mSwipeRefreshLayout.setColorSchemeColors(Color.RED, Color.BLUE, Color.GREEN);
-        mSwipeRefreshLayout.setOnRefreshListener(this);
+        mEmptyBtn = $(R.id.showEmpty);
+        mErrorBtn = $(R.id.showError);
+        mRestoreBtn = $(R.id.restore);
+        mErrorBtn.setOnClickListener(this);
+        mEmptyBtn.setOnClickListener(this);
+        mRestoreBtn.setOnClickListener(this);
+
         mYRecyclerView.setLoadMore(this);
         mYRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mYRecyclerView.addItemDecoration(new YRecyclerViewItemDivider(YRecyclerViewItemDivider.VERTICAL_LIST, Color.BLACK, 5));
-     /* mYRecyclerView.setLayoutManager(new GridLayoutManager(this,2));
-       mYRecyclerView.addItemDecoration(new GridStaggerDivider(this));*/
+        //封装好了的线性分割线
+        mYRecyclerView.addLinearDivider(YRecyclerView.VERTICAL_LIST);
         mYRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        //添加错误ss的View
+        mYRecyclerView.setErrorView(R.layout.error_view);
 
-         dataAdapter=new DataAdapter();
+        dataAdapter = new DataAdapter();
 
 
         List<String> strings = new ArrayList<>();
@@ -47,11 +50,15 @@ public class LoadingActivity extends AppCompatActivity implements YRecyclerView.
             strings.add("数据" + i);
         }
         dataAdapter.addAll(strings);
-
         mYRecyclerView.setAdapter(dataAdapter);
-        View empty = LayoutInflater.from(this).inflate(R.layout.view_empty,null,false);
-        mYRecyclerView.setEmptyView(empty);
-        mYRecyclerView.setEmptyView(empty);
+
+        /*//这个是添加空View
+        View empty = LayoutInflater.from(this).inflate(R.layout.view_empty, null, false);
+        mYRecyclerView.setEmptyView(empty);*/
+
+        mYRecyclerView.setEmptyView(R.layout.view_empty);
+
+       mYRecyclerView.addHeaderView(R.layout.header_view);
     }
 
 
@@ -62,21 +69,21 @@ public class LoadingActivity extends AppCompatActivity implements YRecyclerView.
 
     @Override
     public void loadMore() {
-        Log.i(TAG, "加载更多调用了");
-        mYRecyclerView.setLoadMoreState(YRecyclerView.LOADING);
+        mYRecyclerView.setLoading();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                int count = dataAdapter.getItemCount()+1;
+                int count = dataAdapter.getItemCount() + 1;
                 if (count < 50) {
                     List<String> strings = new ArrayList<>();
                     for (int i = count - 1; i < 5 + count; i++) {
                         strings.add("数据" + i);
                     }
                     dataAdapter.addAll(strings);
-                    mYRecyclerView.setLoadMoreState(YRecyclerView.LOADING_COMPLETE);
+                    mYRecyclerView.setLoadingComplete();
                 } else {
-                    mYRecyclerView.setLoadMoreState(YRecyclerView.LOADING_END);
+
+                    mYRecyclerView.setLoadingNoMore("唉呀妈呀，没数据咯");
                 }
 
 
@@ -85,13 +92,38 @@ public class LoadingActivity extends AppCompatActivity implements YRecyclerView.
     }
 
     @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.showEmpty:
+                dataAdapter.clearAll();
+                break;
+
+            case R.id.showError:
+                mYRecyclerView.showError(true);
+                break;
+            case R.id.restore:
+                dataAdapter.clearAll();
+                List<String> strings = new ArrayList<>();
+                for (int i = 0; i < 30; i++) {
+                    strings.add("数据" + i);
+                }
+
+                mYRecyclerView.reset();
+                dataAdapter.addAll(strings);
+                break;
+        }
+    }
+
+
+
+  /*  @Override
     public void onRefresh() {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 dataAdapter.clearAll();
-                mSwipeRefreshLayout.setRefreshing(false);
+                mYRecyclerView.setRefreshing(false);
             }
         }, 2000);
-    }
+    }*/
 }
