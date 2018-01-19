9ddee9228f4e29baaf03d8d1b3a3eb4e016d1e8f
mr.gao <1451953172@qq.com>
2018-01-19 23:16:19 +0800

    使用RecyclerView：比如自定义分割线，自定义动画，添加healderView,footerView;上啦加载更多，下拉刷新

---
+++ textlab/src/main/java/com/mrgao/textlab/MyLibText.java
@@ -0,0 +1,15 @@
+package com.mrgao.textlab;
+
+/**
+ * Created by mr.gao on 2018/1/19.
+ * Package:    com.mrgao.textlab
+ * Create Date:2018/1/19
+ * Project Name:RecyclerViewText
+ * Description:
+ */
+
+public class MyLibText {
+    public int add(int a,int b){
+        return a+b;
+    }
+}
